c2215334172daba130a969730bfeb2b04ecdc208
Caroline Araujo <ca.araujosouza@gmail.com>
2021-01-06 14:45:57 -0300

    add lambdas

--- src/br/com/bytebank/banco/test/util/TesteClasseAnonima.java
+++ src/br/com/bytebank/banco/test/util/TesteClasseAnonima.java
@@ -4,13 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 
 import br.com.bytebank.banco.modelo.Cliente;
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 
-public class Teste {
+public class TesteClasseAnonima {
 
 	public static void main(String[] args) {
 
@@ -44,17 +43,31 @@ public class Teste {
 		lista.add(cc3);
 		lista.add(cc4);
 
-		lista.sort( (c1, c2) -> Integer.compare(c1.getNumero(), c2.getNumero()));
+		lista.sort(new Comparator<Conta>() { // classe anonima
+
+			@Override
+			public int compare(Conta c1, Conta c2) {
+
+				return Integer.compare(c1.getNumero(), c2.getNumero());
+
+				}
+			}
 		
-		Comparator<Conta> comp = (Conta c1, Conta c2) -> {
+		);
+		
+		Comparator<Conta> comp = new Comparator<Conta>() {
+			
+			@Override
+			public int compare(Conta c1, Conta c2) {
 				String nomeC1 = c1.getTitular().getNome();
 				String nomeC2 = c2.getTitular().getNome();
 				return nomeC1.compareTo(nomeC2);
+			}
 		};
-		
-		lista.sort(comp);
 
-		lista.forEach( (conta) -> System.out.println(conta + ", " + conta.getTitular().getNome()));
+		for (Conta conta : lista) {
+			System.out.println(conta + ", " + conta.getTitular().getNome());
+		}
 
 	}
 
